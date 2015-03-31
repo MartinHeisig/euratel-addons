4cddcbe26e1aaa6314e767d004ea23c21d823de7
Martin Apitz <it@artmin.de>
2015-03-31 11:10:12 +0200

    Changed Sales Order and Invoice to new design. Extended Sales Team by multiple fields which are used in reports.

--- euratel_custom_fields/sale.py
+++ euratel_custom_fields/sale.py
@@ -20,12 +20,41 @@
 ##############################################################################
 
 from openerp.osv import osv, fields
+from openerp import tools, api
 
 class amamedis_sales_team(osv.osv):
 
     _inherit = 'crm.case.section'
     _description = "Add custom fields for Amamedis sales team."
+    
+    @api.multi
+    def _get_image(self, name, args):
+        return dict((p.id, tools.image_get_resized_images(p.image)) for p in self)
+
+    @api.one
+    def _set_image(self, name, value, args):
+        return self.write({'image': tools.image_resize_image_big(value)})
 
     _columns = {
-            'company_id' : fields.many2one('res.company', 'Unternehmen'),
+            'from_email' : fields.char('Absenderemail', help="Emailadresse mit der Emails aus dem System versand werden."),
+            'from_line' : fields.char('Absenderzeile', help="Adresszeile für ausgehende Dokumente"),
+            'footer' : fields.html('Fußzeile', help="Fußzeile für externe Dokumente"),
+            'contact' : fields.html('Ansprechpartner', help="Ansprechpartner welche in ausgehenden Dokumenten angezeigt werden."),
+            'closing' : fields.char('Briefabschluss'),
+            'image' : fields.binary('Logo'),
+            'image_medium' : fields.function(_get_image, fnct_inv=_set_image,
+                string="Medium-sized image", type="binary", multi="_get_image",
+                store={
+                    'crm.case.section': (lambda self, cr, uid, ids, c={}: ids, ['image'], 10),
+                    },),
     }
+
+class amamedis_sale_order(osv.osv):
+
+    _inherit = 'sale.order'
+    _description = "Add custom fields to sale order."
+
+    _columns = {
+            'delivery_date' : fields.char('Lieferdatum'),
+            'client_order_date' : fields.date('Bestelldatum'),
+            }
