06c495ef687bcbdf6efffc9330d01db021c8a3f0
Martin Apitz <it@artmin.de>
2015-06-23 16:52:31 +0200

    Added city to view of partner in many2one listbox.

--- euratel_custom_fields/partner.py
+++ euratel_custom_fields/partner.py
@@ -39,3 +39,30 @@ class euratel_partner(osv.osv):
             'branch_id',
             'Filialen'),
     }
+
+    ''' Adds city to all displays of partners and gives possibility to show city
+    in many2one relation. '''
+    def name_get(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        res = []
+        for record in self.browse(cr, uid, ids, context=context):
+            name = record.name
+            if record.parent_id and not record.is_company:
+                name = "%s, %s" % (record.parent_name, name)
+            if record.city:
+                name += ' (' + record.city + ')'
+            if context.get('show_address_only'):
+                name = self._display_address(cr, uid, record, without_company=True, context=context)
+            if context.get('show_address'):
+                name = name + "\n" + self._display_address(cr, uid, record, without_company=True, context=context)
+            if context.get('show_street') and record.street:
+                name += "\n" + record.street
+            name = name.replace('\n\n','\n')
+            name = name.replace('\n\n','\n')
+            if context.get('show_email') and record.email:
+                name = "%s <%s>" % (name, record.email)
+            res.append((record.id, name))
+        return res
