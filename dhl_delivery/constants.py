9344b8200896db6047f4b7176deaa337226c8aff
Martin Apitz <it@artmin.de>
2015-08-25 07:44:56 +0200

    Added constants for dhl delivery.

---
+++ dhl_delivery/constants.py
@@ -0,0 +1,37 @@
+# constants for creating arguments
+
+# Sender
+SH_COMPANY_NAME = "Shipper_company_name";
+SH_STREET = "Shipper_street";
+SH_STREET_NR = "Shipper_street_nr";
+SH_CITY = "Shipper_city";
+SH_ZIP = "Shipper_zip";
+SH_CONTACT_EMAIL = "Shipper_contact_email";
+SH_CONTACT_NAME = "Shipper_contact_name";
+SH_CONTACT_PHONE = "Shipper_contact_phone";
+
+# Receiver
+RC_CONTACT_EMAIL = "Receiver_contact_email"
+RC_CONTACT_NAME = "Receiver_contact_name"
+RC_CONTACT_PHONE = "Receiver_contact_phone"
+RC_COMPANY_NAME = "Receiver_company_name"
+RC_FIRST_NAME = "Receiver_first_name"
+RC_LAST_NAME = "Receiver_last_name"
+RC_LOCAL_CITY = "Receiver_local_city"
+RC_LOCAL_STREET = "Receiver_local_street"
+RC_LOCAL_STREETNR = "Receiver_local_streetnr"
+RC_LOCAL_ZIP = "Receiver_local_zip"
+NUMBER_OF_SHIPMENTS = "Number_of_shipments"
+
+# Other
+SHIPPING_NUMBER = "Shipping_number"
+METHOD = "Method"
+TEST = "Test"
+
+# Credentials
+INTRASHIP_USER = "Intraship_user"
+INTRASHIP_PASSWORD = "Intraship_password"
+EKP = "EKP"
+PARTNER_ID = "Partner_ID"
+
+
