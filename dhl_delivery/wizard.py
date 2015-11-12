348ed50890c1d146d1e01f96082b2b3d4d4f1d65
Martin Heisig <m.heisig@gmx.net>
2015-11-12 16:46:38 +0100

    added .strip() to address splitting added UTF-8 encode to most of the used strings

--- dhl_delivery/wizard.py
+++ dhl_delivery/wizard.py
@@ -65,7 +65,7 @@ class DHLStockTransferDetails(models.TransientModel):
                     parcels += int(ceil(quantity / pcs_per_box))
                 else:
                      raise osv.except_osv(('Fehler'), ('Fuer das Produkt \"' +
-                         item.product_id.name + '\" ist keine Gebindegröße hinterlegt.'))
+                         item.product_id.name.encode('utf-8') + '\" ist keine Gebindegröße hinterlegt.'))
             # Error handling
             if parcels == 0:
                 raise osv.except_osv(('Fehler'), ('Die Anzahl der berechneten '
@@ -85,33 +85,34 @@ class DHLStockTransferDetails(models.TransientModel):
             # Prepare call of Java tool
             # Divide street and street number for sender and reciever
             street_as_list = sender.street.split(' ')
-            sh_street = ' '.join(street_as_list[:-1])
-            sh_street_nr = street_as_list[-1]
+            sh_street = ' '.join(street_as_list[:-1]).strip()
+            sh_street_nr = street_as_list[-1].strip()
             street_as_list = self.picking_id.partner_id.street.split(' ')
-            rc_street = ' '.join(street_as_list[:-1])
-            rc_street_nr = street_as_list[-1]
+            rc_street = ' '.join(street_as_list[:-1]).strip()
+            rc_street_nr = street_as_list[-1].strip()
             company = self.picking_id.company_id
+            # raise osv.except_osv('Mein Fehlertest', parcels)
             # Set arguments
             vals = {
                     # Reciever details
-                    RC_CONTACT_EMAIL : self.picking_id.partner_id.email,
-                    RC_CONTACT_PHONE : self.picking_id.partner_id.phone,
-                    RC_COMPANY_NAME : self.picking_id.partner_id.name,
-                    RC_COMPANY_NAME_2 : self.picking_id.partner_id.first_name,
-                    RC_LOCAL_CITY : self.picking_id.partner_id.city,
-                    RC_LOCAL_STREET : rc_street,
-                    RC_LOCAL_STREETNR : rc_street_nr,
-                    RC_LOCAL_ZIP : self.picking_id.partner_id.zip,
+                    RC_CONTACT_EMAIL : self.picking_id.partner_id.email.encode('utf-8'),
+                    RC_CONTACT_PHONE : self.picking_id.partner_id.phone.encode('utf-8'),
+                    RC_COMPANY_NAME : self.picking_id.partner_id.name.encode('utf-8'),
+                    RC_COMPANY_NAME_2 : self.picking_id.partner_id.first_name.encode('utf-8'),
+                    RC_LOCAL_CITY : self.picking_id.partner_id.city.encode('utf-8'),
+                    RC_LOCAL_STREET : rc_street.encode('utf-8'),
+                    RC_LOCAL_STREETNR : rc_street_nr.encode('utf-8'),
+                    RC_LOCAL_ZIP : self.picking_id.partner_id.zip.encode('utf-8'),
                     NUMBER_OF_SHIPMENTS : str(parcels),
-                    CUSTOMER_REFERENCE : self.picking_id.name,
+                    CUSTOMER_REFERENCE : self.picking_id.name.encode('utf-8'),
                     # Sender details
-                    SH_COMPANY_NAME : sender.name,
-                    SH_STREET : sh_street,
-                    SH_STREET_NR : sh_street_nr,
-                    SH_CITY : sender.city,
-                    SH_ZIP : sender.zip,
-                    SH_CONTACT_EMAIL : sender.email,
-                    SH_CONTACT_PHONE : sender.phone,
+                    SH_COMPANY_NAME : sender.name.encode('utf-8'),
+                    SH_STREET : sh_street.encode('utf-8'),
+                    SH_STREET_NR : sh_street_nr.encode('utf-8'),
+                    SH_CITY : sender.city.encode('utf-8'),
+                    SH_ZIP : sender.zip.encode('utf-8'),
+                    SH_CONTACT_EMAIL : sender.email.encode('utf-8'),
+                    SH_CONTACT_PHONE : sender.phone.encode('utf-8'),
                     # Options and Credentials
                     METHOD : 'createShipment',
                     TEST : company.dhl_test and 'True' or False,
