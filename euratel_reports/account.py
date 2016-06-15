0c96d089b7e4fb8b582692b7ac47f378b48c2eaf
Martin Heisig <m.heisig@gmx.net>
2016-06-15 15:17:59 +0200

    changed the build from payment term lines on invoice

--- euratel_reports/account.py
+++ euratel_reports/account.py
@@ -5,10 +5,10 @@ from openerp.exceptions import except_orm, Warning
 class ama_account_payment_term(models.Model):
     _inherit = "account.payment.term"
     
-    name_on_invoice = fields.Boolean('Name auf Rechnung', default=True, help='Bei gesetztem Haken erscheint zusaetzlich zu den Zeilen unten der Name der Zahlungsbedingung auf der Rechnung. Wenn abgewaehlt, dann nur die Zeilen unten.')
+    name_on_invoice = fields.Boolean('Name Zahlungsbedingung auf Rechnung', default=True, help='Bei gesetztem Haken erscheint zusaetzlich zu den Zeilen unten der Name der Zahlungsbedingung auf der Rechnung. Wenn abgewaehlt, dann nur die Zeilen unten.')
 
 
 class ama_account_payment_term_line(models.Model):
     _inherit = "account.payment.term.line"
 
-    report_text = fields.Char('Report-Text', required=True)
+    report_text = fields.Text('Report-Text', required=True)
